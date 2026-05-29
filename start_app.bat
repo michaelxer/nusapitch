cf368025ac9690751544a9fc78ae3b1939b78820
michaelxer <michaelxer@users.noreply.github.com>
2026-05-29 16:21:57 +0700

    fix: disable streamlit onboarding prompt

--- start_app.bat
+++ start_app.bat
@@ -6,6 +6,7 @@ if not exist ".venv\Scripts\python.exe" (
 )
 
 call ".venv\Scripts\activate.bat"
+set STREAMLIT_BROWSER_GATHER_USAGE_STATS=false
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 streamlit run app\streamlit_app.py
